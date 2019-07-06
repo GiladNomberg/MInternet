4fca9d56209a3d22be2eee3c208a5aa3e5e8e860
Gilad <nomberg7@gmail.com>
2019-07-06 14:04:07 +0300

    update app.py, .gitignore

--- backend/app.py
+++ backend/app.py
@@ -3,19 +3,14 @@ import re
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-
 DEBUG = True
-
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-
 CORS(app)
 
 customers = []
-
-df = pd.read_csv('churn_case_data.csv',sep = ';')
-for index,row in df.iterrows():
+df = pd.read_csv('churn_case_data.csv', sep = ';')
+for index, row in df.iterrows():
 	customer_dict = {}
 	customer_dict['id'] = row['id']
 	customer_dict['gender'] = row['gender']
@@ -25,6 +20,7 @@ for index,row in df.iterrows():
 	customer_dict['contract_value'] = row['contract_value']
 	customers.append(customer_dict)	
 	
+
 @app.route("/")
 def get_data():
 	response_object = {}
@@ -32,7 +28,7 @@ def get_data():
 	isDigit = re.search("\d", _id)
 	if isDigit:
 		segment_id = int(_id[isDigit.start()])
-	else:	
+	else:
 		segment_id = 0
 	if(segment_id != 0):
 		specificCustomers = []
@@ -40,12 +36,13 @@ def get_data():
 			if(customer["segment"] == segment_id):
 				specificCustomers.append(customer)
 		response_object["data"] = specificCustomers
-		add_meta_data(response_object,specificCustomers)	
+		add_meta_data(response_object, specificCustomers)	
 	else:
 		response_object["data"] = customers
-		add_meta_data(response_object,customers)
+		add_meta_data(response_object, customers)
 		
-	return jsonify(response_object)	
+	return jsonify(response_object)
+
 	
 def add_meta_data(response_object, customers):
 	sum_years = 0
@@ -55,15 +52,14 @@ def add_meta_data(response_object, customers):
 	for customer in customers:
 		sum_years += customer["years_customer"]
 		sum_complains += customer["no_of_complaints"]
-		number_of_customers = number_of_customers + 1
-		
-	average_years = sum_years/number_of_customers
+		number_of_customers += 1
+	
+	average_years = sum_years / number_of_customers
 	average_years = ("%.4f" % average_years)
-	average_complains = sum_complains/number_of_customers
+	average_complains = sum_complains / number_of_customers
 	average_complains = ("%.4f" % average_complains)
 	response_object["years"] = average_years
 	response_object["complains"] = average_complains
-	
+
 if __name__ == "__main__":
-    app.run()	
-	
+	app.run()
